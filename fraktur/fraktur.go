35260e73afb138079232e43ac201ec6ea26dc23e
Kyle Huggins <kyle@huggins.io>
2026-03-23 13:15:13 -0800

    refactor to make generic

--- fraktur/fraktur.go
+++ fraktur/fraktur.go
@@ -11,8 +11,28 @@ import (
 	"github.com/charmbracelet/x/exp/charmtone"
 )
 
-func CharmFangColorSchemeFunc(c lipgloss.LightDarkFunc) fang.ColorScheme {
-	return fang.ColorScheme{
+// Theme holds the Fraktur color palette as raw Lip Gloss colors, usable in
+// any Lip Gloss or Bubble Tea project.
+type Theme struct {
+	Base           color.Color
+	Title          color.Color
+	Codeblock      color.Color
+	Program        color.Color
+	Command        color.Color
+	DimmedArgument color.Color
+	Comment        color.Color
+	Flag           color.Color
+	Argument       color.Color
+	Description    color.Color
+	FlagDefault    color.Color
+	QuotedString   color.Color
+	ErrorHeader    [2]color.Color // 0=fg, 1=bg
+}
+
+// NewTheme returns the Fraktur color palette resolved for the given light/dark
+// context. Use [charm.land/lipgloss/v2.LightDark] to create c.
+func NewTheme(c lipgloss.LightDarkFunc) Theme {
+	return Theme{
 		Base:           c(charmtone.Charcoal, charmtone.Ash),
 		Title:          charmtone.Charple,
 		Codeblock:      c(charmtone.Salt, lipgloss.Color("#2F2E36")),
@@ -22,12 +42,32 @@ func CharmFangColorSchemeFunc(c lipgloss.LightDarkFunc) fang.ColorScheme {
 		Comment:        c(charmtone.Squid, lipgloss.Color("#747282")),
 		Flag:           c(lipgloss.Color("#0CB37F"), charmtone.Guac),
 		Argument:       c(charmtone.Charcoal, charmtone.Ash),
-		Description:    c(charmtone.Charcoal, charmtone.Ash), // flag and command descriptions
-		FlagDefault:    c(charmtone.Smoke, charmtone.Squid),  // flag default values in descriptions
+		Description:    c(charmtone.Charcoal, charmtone.Ash),
+		FlagDefault:    c(charmtone.Smoke, charmtone.Squid),
 		QuotedString:   c(charmtone.Coral, charmtone.Salmon),
 		ErrorHeader: [2]color.Color{
 			charmtone.Butter,
 			charmtone.Cherry,
 		},
+	}
+}
+
+// CharmFangColorSchemeFunc returns the Fraktur theme as a [charm.land/fang/v2.ColorScheme].
+func CharmFangColorSchemeFunc(c lipgloss.LightDarkFunc) fang.ColorScheme {
+	t := NewTheme(c)
+	return fang.ColorScheme{
+		Base:           t.Base,
+		Title:          t.Title,
+		Codeblock:      t.Codeblock,
+		Program:        t.Program,
+		Command:        t.Command,
+		DimmedArgument: t.DimmedArgument,
+		Comment:        t.Comment,
+		Flag:           t.Flag,
+		Argument:       t.Argument,
+		Description:    t.Description,
+		FlagDefault:    t.FlagDefault,
+		QuotedString:   t.QuotedString,
+		ErrorHeader:    t.ErrorHeader,
 	}
 }
